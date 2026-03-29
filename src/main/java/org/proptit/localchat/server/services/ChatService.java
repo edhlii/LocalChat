576ca7163dff0abb4efe571e7f3ef5468068df86
Nguyen Van Quang <vanquang120406@gmail.com>
2026-03-29 15:40:10 +0700

    SocketClient and Message's Model

--- src/main/java/org/proptit/localchat/server/services/ChatService.java
+++ src/main/java/org/proptit/localchat/server/services/ChatService.java
@@ -1,4 +1,26 @@
 package org.proptit.localchat.server.services;
 
+import org.proptit.localchat.server.controller.ClientHandler;
+import org.proptit.localchat.server.networks.SocketServer;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatService {
+    private List<ClientHandler> clients;
+
+    public ChatService(List<ClientHandler> clients) {
+        this.clients = clients;
+    }
+
+    public void sendToAll(Object message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message.toString());
+        }
+    }
+
+    public void sendToPerson(Object message) {
+
+    }
 }
