df44ab842a9e8b9b8c82fc0849993516204aae7f
Le Dinh Hieu <dhgamer1811@gmail.com>
2026-04-02 09:28:22 +0700

    feat: added basic chat window ui

--- src/main/java/org/proptit/localchat/ChatWindow.java
+++ src/main/java/org/proptit/localchat/ChatWindow.java
@@ -12,7 +12,7 @@ public class ChatWindow extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChatWindow.class.getResource("chat_window.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("LocalChat");
         stage.setScene(scene);
         stage.show();
     }
