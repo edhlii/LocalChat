1fc3be40bd3572fc9c2b3a93b5125e3e77b8faa6
Le Dinh Hieu <dhgamer1811@gmail.com>
2026-04-13 16:02:03 +0700

    ui: complete call window ui

--- src/main/java/org/proptit/localchat/client/controller/CallWindowController.java
+++ src/main/java/org/proptit/localchat/client/controller/CallWindowController.java
@@ -1,4 +1,44 @@
 package org.proptit.localchat.client.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 public class CallWindowController {
+
+    @FXML
+    private Button btnAddParticipant;
+
+    @FXML
+    private Button btnCamera;
+
+    @FXML
+    private Button btnChat;
+
+    @FXML
+    private Button btnEndCall;
+
+    @FXML
+    private Button btnMic;
+
+    @FXML
+    private Button btnShareScreen;
+
+    @FXML
+    private Button btnToggleView;
+
+    @FXML
+    private Label callStatusLabel;
+
+    @FXML
+    private Label contactNameLabel;
+
+    @FXML
+    private StackPane localPreviewPane;
+
+    @FXML
+    private Pane remoteVideoPane;
+
 }
