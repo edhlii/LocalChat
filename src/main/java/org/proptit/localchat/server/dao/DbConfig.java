2b5ee254f0a2d41184ca83af5f532bcb2aecae49
nguyen duc anh <ducanh9109vip@gmail.com>
2026-04-21 23:24:54 +0700

    hide my info password

--- src/main/java/org/proptit/localchat/server/dao/DbConfig.java
+++ src/main/java/org/proptit/localchat/server/dao/DbConfig.java
@@ -3,8 +3,8 @@ package org.proptit.localchat.server.dao;
 public class DbConfig {
     public static String driver = "com.mysql.cj.jdbc.Driver";
     public static String url = "jdbc:mysql://localhost:3306/localchat";
-    public static String user = "root";
+    public static String user = "";
     //anh em tu dat mat khau vao day
-    public static String password = "ducanh9109pinssd";
+    public static String password = "";
 
 }
