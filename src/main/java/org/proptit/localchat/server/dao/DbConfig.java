9b1dcf04769d61de4ed12cb01cb492cf33f6363b
Nguyen Van Quang <vanquang120406@gmail.com>
2026-04-25 23:20:25 +0700

    feature/create-group-ver2

--- src/main/java/org/proptit/localchat/server/dao/DbConfig.java
+++ src/main/java/org/proptit/localchat/server/dao/DbConfig.java
@@ -2,9 +2,9 @@ package org.proptit.localchat.server.dao;
 
 public class DbConfig {
     public static String driver = "com.mysql.cj.jdbc.Driver";
-    public static String url = "jdbc:mysql://localhost:3306/localchat";
+    public static String url = "jdbc:mysql://localhost:3306/locarootlchat";
     public static String user = "vanq";
     //anh em tu dat mat khau vao day
-    public static String password = "Quang120406.";
+    public static String password = "xxxxx";
 
 }
