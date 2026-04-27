f58c1e4ec387fe90dfe4861ef4fa4a393503e927
Nguyen Van Quang <vanquang120406@gmail.com>
2026-04-27 22:37:57 +0700

    update/ui

--- src/main/java/org/proptit/localchat/server/dao/DbConfig.java
+++ src/main/java/org/proptit/localchat/server/dao/DbConfig.java
@@ -3,10 +3,10 @@ package org.proptit.localchat.server.dao;
 public class DbConfig {
     public static String driver = "com.mysql.cj.jdbc.Driver";
     public static String url = "jdbc:mysql://localhost:3306/localchat";
-    public static String user = "vanq";
+    public static String user = "xxx";
     //anh em tu dat mat khau vao day
 
-    public static String password = "Quang120406.";
+    public static String password = "xxx";
 
 
 }
