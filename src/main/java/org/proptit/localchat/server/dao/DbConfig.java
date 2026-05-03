f7a6c91b70631d93c589293bfac40db3ef147ec7
nguyen duc anh <ducanh9109vip@gmail.com>
2026-05-03 12:59:18 +0700

    fix bugs

--- src/main/java/org/proptit/localchat/server/dao/DbConfig.java
+++ src/main/java/org/proptit/localchat/server/dao/DbConfig.java
@@ -4,6 +4,6 @@ public class DbConfig {
     public static String driver = "com.mysql.cj.jdbc.Driver";
     public static String url = "jdbc:mysql://localhost:3306/localchat";
     public static String user = "root";
-    public static String password = "ducanh9109pinssd";
+    public static String password = "xxx";
 
 }
