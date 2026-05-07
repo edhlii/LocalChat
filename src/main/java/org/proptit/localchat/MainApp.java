79981082f011db01fee87dc1c4f7e539ceefd882
Nguyen Van Quang <vanquang120406@gmail.com>
2026-05-07 20:29:03 +0700

    Config-ip-server

--- src/main/java/org/proptit/localchat/MainApp.java
+++ src/main/java/org/proptit/localchat/MainApp.java
@@ -6,17 +6,34 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.proptit.localchat.client.controller.LoginController;
 import org.proptit.localchat.client.networks.SocketClient;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("login_view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/proptit/localchat/login_view.fxml"));
         Scene scene = new Scene(loader.load());
 
         LoginController loginController = loader.getController();
 
+        Properties prop = new Properties();
+        String host = "127.0.0.1";
+        int port = 1204;
 
-        SocketClient client = new SocketClient("127.0.0.1", 1204, null);
+        try (InputStream input = new FileInputStream("server.properties")) {
+            prop.load(input);
+            host = prop.getProperty("server_ip", "127.0.0.1");
+            port = Integer.parseInt(prop.getProperty("server_port", "1204"));
+            System.out.println("FINDED");
+        } catch (IOException ex) {
+            System.out.println("Không tìm thấy file config, dùng IP mặc định.");
+        }
+
+        SocketClient client = new SocketClient(host, port, null);
+
         client.setLoginController(loginController);
         loginController.setSocketClient(client);
 
